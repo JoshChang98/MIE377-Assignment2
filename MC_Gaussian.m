00bfed85680f476d3c515098860fc236d5ec09a5
JoshChang98 <47726243+JoshChang98@users.noreply.github.com>
2019-03-30 12:16:50 -0400

    Add files via upload

--- MC_Gaussian.m
+++ MC_Gaussian.m
@@ -1,16 +1,16 @@
-%function [S] = MC_Gaussian(initPrice,mu_FF, Q_FF)
+function [S] = MC_Gaussian(initPrice,mu_FF, Q_FF)
 clc
 clear all
 format long
 
-%%%%%%%%%%TEST PARAMETERS DELETE THIS CODE ONCE FINISHED TESTING
-% Stock  parameters (weekly)
-initPrice    = [100; 150];          % Initial price of stock A
-mu_FF    = [0.1 / 100; 0.2/100];    % expected return
-Q_FF = [0.000064 0.0000576; 0.0000576 0.000144];    % covariance matrix
-
-
-%%%%%%%%%%TEST PARAMETERS DELETE THIS CODE ONCE FINISHED TESTING
+% %%%%%%%%%%TEST PARAMETERS DELETE THIS CODE ONCE FINISHED TESTING
+% % Stock  parameters (weekly)
+% initPrice    = [100; 150];          % Initial price of stock A
+% mu_FF    = [0.1 / 100; 0.2/100];    % expected return
+% Q_FF = [0.000064 0.0000576; 0.0000576 0.000144];    % covariance matrix
+% 
+% 
+% %%%%%%%%%%TEST PARAMETERS DELETE THIS CODE ONCE FINISHED TESTING
 
 %This function calculates stock price scenarios using the Gaussian Monte
 %Carlo simulations
@@ -24,7 +24,7 @@ T   = 52 * numbYear;       % Time window
 N   = 52 * numbYear;      % Number of steps (half a week per time step)
 dt  = T / N;    % Timestep 
 n =  size(mu_FF,1);%number of assets
-sigmaQ = diag(Q_FF); %standard deviations of assets
+sigmaQ = sqrt(diag(Q_FF)); %standard deviations of assets
 
 % Number of simulated price paths
 nPaths = 5000;
@@ -49,18 +49,18 @@ for k = 1: n;
    S_temp(1,:) = initPrice(k);
    S{k} = S_temp;
 end
+S{k}(2,1)
 for i = 1:nPaths
     for j = 1:N
         
         % Convert the independent RVs to correlated RVs
         xi = L * randn(2, 1); 
+       
         for k = 1 : n
         
         S{k}(j+1, i) = S{k}(j, i) * exp( ( mu_FF(k) - 0.5 * (sigmaQ(k))^2 ) * dt ...
                         + sigmaQ(k) * sqrt(dt) * xi(k) );
         
-        %S_B(j+1, i) = S_B(j, i) * exp( ( mu_B - 0.5 * sigma_B^2 ) * dt ...
-        %                + sigma_B * sqrt(dt) * xi(2) );
         end  
     end
 end 
